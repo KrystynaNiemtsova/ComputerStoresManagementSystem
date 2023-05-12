fef872af3f590fe7186a0424e016470699cdc51d
Krystyna Niemtsova <kniemtsova@gmail.com>
2023-05-12 15:49:14 +1200

    - Added 5 more classes. DatabaseFileReader: reads in the database file. DesktopPC: child class of the device parent class, and holds information specific to Desktop Pc's including memory size and SSD. Laptop: a child class of the DesktopPC parent class, and holds information specific to laptops including screen size. Tablet: a child class of the device parent class, and holds information specific to tablets including screen size. Device: This class is parent class. It is the parent class of Tablet, Desktop PC and Laptop, and it holds the information relevant to these three different types of computer including its category, type, ID, brand, CPU and price.

---
+++ src/DatabaseFileReader.java
@@ -0,0 +1,12 @@
+import java.io.File;
+import java.util.ArrayList;
+
+public class DatabaseFileReader {
+    private static ArrayList<DesktopPC> desktopPC = new ArrayList<>();
+    private static ArrayList<Laptop> laptop = new ArrayList<>();
+    private static ArrayList<Tablet> tablet = new ArrayList<>();
+    private static ArrayList<Device> allDevices = new ArrayList<>();
+    public DatabaseFileReader(File infile) {
+
+    }
+}
