a7d99d646b9925b00554e9638a177ac38dfadc53
Krystyna Niemtsova <kniemtsova@gmail.com>
2023-05-15 11:00:13 +1200

    - Added three more classes: Users, ComputerTable and LoginDialogScreen.

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,13 @@ import java.io.File;
 public class Main {
     public static void main(String[] args) {
         new DatabaseFileReader(new File("computers.txt"));
+        setUserInformation();
+    }
+    private static void setUserInformation() {
+        LoginDialogScreen.setUsers("p1", "p1", "Salesperson");
+        LoginDialogScreen.setUsers("p2", "p2", "Salesperson");
+        LoginDialogScreen.setUsers("p3", "p3", "Salesperson");
+        LoginDialogScreen.setUsers("m1", "m1", "Manager");
+        LoginDialogScreen.setUsers("m2", "m2", "Manager");
     }
 }
