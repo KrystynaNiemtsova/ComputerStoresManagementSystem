5ac8b39d1a2f6976d5da8e0b55ab7396751acc89
Krystyna Niemtsova <kniemtsova@gmail.com>
2023-05-17 17:50:20 +1200

    Implemented DesktopPC class which extends the Device class. The DesktopPC class introduces additional properties such as memorySize and SSD (solid-state drive). It provides a constructor to initialize these properties along with the inherited properties from the Device class. Additionally, it includes getter and setter methods for memorySize and SSD.

--- src/DesktopPC.java
+++ src/DesktopPC.java
@@ -1,2 +1,21 @@
-public class DesktopPC {
+public class DesktopPC extends Device {
+    private int memorySize;
+    private int SSD;
+    public DesktopPC(String category, String type, String ID, String brand, String CPU, int memorySize, int SSD, float price) {
+        super(category, type, ID, brand, CPU, price);
+        setMemorySize(memorySize);
+        setSSD(SSD);
+    }
+    public int getMemorySize() {
+        return memorySize;
+    }
+    public void setMemorySize(int memorySize) {
+        this.memorySize = memorySize;
+    }
+    public int getSSD() {
+        return SSD;
+    }
+    public void setSSD(int SSD) {
+        this.SSD = SSD;
+    }
 }
