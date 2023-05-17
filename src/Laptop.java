59001ee583f88126734c617ed66b9599bd30081b
Krystyna Niemtsova <kniemtsova@gmail.com>
2023-05-17 17:52:39 +1200

    Implemented Laptop class, which extends the DesktopPC class. The Laptop class introduces an additional property, screenSize, which represents the size of the laptop's screen. It provides a constructor to initialize this property along with the inherited properties from the DesktopPC class. Additionally, it includes getter and setter methods for the screenSize property.

--- src/Laptop.java
+++ src/Laptop.java
@@ -1,2 +1,14 @@
-public class Laptop {
+public class Laptop extends DesktopPC {
+    private float screenSize;
+    public Laptop(String category, String type, String ID, String brand, String CPU, int memorySize, int SSD,
+                  float ScreenSize, float price) {
+        super(category, type, ID, brand, CPU, memorySize, SSD, price);
+        setScreenSize(screenSize);
+    }
+    public float getScreenSize() {
+        return screenSize;
+    }
+    public void setScreenSize(float screenSize) {
+        this.screenSize = screenSize;
+    }
 }
