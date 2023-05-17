05acb8a24fb17fb10959f770e8b4ce68cfd50dc6
Krystyna Niemtsova <kniemtsova@gmail.com>
2023-05-17 17:59:13 +1200

    Implemented Tablet class,  which extends the Device class. The Tablet class introduces an additional property, screenSize, which represents the size of the tablet's screen. It provides a constructor to initialize this property along with the inherited properties from the Device class. Additionally, it includes getter and setter methods for the screenSize property.

--- src/Tablet.java
+++ src/Tablet.java
@@ -1,2 +1,13 @@
-public class Tablet {
+public class Tablet  extends Device{
+    private float screenSize;
+    public Tablet(String category, String type, String ID, String brand, String CPU, float screenSize, float price) {
+        super(category, type, ID, brand, CPU, price);
+        setScreenSize(screenSize);
+    }
+    public float getScreenSize() {
+        return screenSize;
+    }
+    public void setScreenSize(float screenSize) {
+        this.screenSize = screenSize;
+    }
 }
