ec2ea7affd2396336e6ad108ff781417e5130fc1
Krystyna Niemtsova <kniemtsova@gmail.com>
2023-05-17 17:41:34 +1200

    Added ComputerTable class with JTabbedPane, ArrayLists, and JFrame.

--- src/ComputerTable.java
+++ src/ComputerTable.java
@@ -1,2 +1,11 @@
-public class ComputerTable {
+import javax.swing.*;
+import java.util.ArrayList;
+
+public class ComputerTable extends JFrame {
+    static JTabbedPane tabbedPane = new JTabbedPane();
+    static ArrayList<String> categories;
+    static ArrayList<String> types;
+    private JPanel browseProducts = new JPanel();
+    protected static JFrame frame;
+    static ComputerTableModel computerTable;
 }
