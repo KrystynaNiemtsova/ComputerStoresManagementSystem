3a6cc7d0d8f7ee6295dda5c68c5f378153da0ad4
Krystyna Niemtsova <kniemtsova@gmail.com>
2023-05-19 17:50:06 +1200

    Added categoryFilter class with category filtering functionality.

---
+++ src/categoryFilter.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class categoryFilter extends RowFilter {
+    private String category;
+    public categoryFilter(String category) {
+        setCategory(category);
+    }
+        @Override
+    public boolean include(Entry entry) {
+        return entry.getStringValue(0).equals(category);
+    }
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
