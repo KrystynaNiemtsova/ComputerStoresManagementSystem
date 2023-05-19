d45a6bb578181d42c7ee3959d0f7dc09c6adcd6a
Krystyna Niemtsova <kniemtsova@gmail.com>
2023-05-19 17:55:45 +1200

    Added typeFilter class with type and category filtering functionality.

---
+++ src/typeFilter.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+
+public class typeFilter extends RowFilter {
+    private String type;
+    private String category;
+    public typeFilter(String type, String category) {
+        setType(type);
+        setCategory(category);
+    }
+    @Override
+    public boolean include (Entry entry) {
+        if (entry.getStringValue(0).equals(category)) {
+            return entry.getStringValue(1).equals(type);
+        }
+        return false;
+    }
+    public void setType(String type) {
+        this.type = type;
+    }
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
