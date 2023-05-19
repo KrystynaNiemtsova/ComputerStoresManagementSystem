7eee662a8367604696658720146ce1ecc189c008
Krystyna Niemtsova <kniemtsova@gmail.com>
2023-05-19 17:56:57 +1200

    Implemented UpdateProducts class for managing product updates.

--- src/UpdateProducts.java
+++ src/UpdateProducts.java
@@ -1,2 +1,31 @@
-public class UpdateProducts {
+import javax.swing.*;
+
+public class UpdateProducts extends JPanel {
+    Device selectedRow;
+    JButton addButton;
+    JButton updateButton;
+    JButton deleteButton;
+    JButton clearButton;
+    JPanel productInfo;
+    JLabel modelIDLabel;
+    JLabel categoryLabel;
+    JLabel typeLabel;
+    JLabel brandLabel;
+    JLabel CPUFamilyLabel;
+    JLabel priceLabel;
+    JLabel memorySizeLabel;
+    JLabel SSDCapacityLabel;
+    JTextField memoryField;
+    JTextField SSDField;
+    JLabel screenSizeLabel;
+    JTextField screeSizeLabel;
+    JTextField modelField;
+    JComboBox<String> categoryCombo;
+    JComboBox<String> typeCombo;
+    JTextField brandField;
+    JTextField CPUField;
+    JTextField priceField;
+    public UpdateProducts(Object givenValue) {
+
+    }
 }
