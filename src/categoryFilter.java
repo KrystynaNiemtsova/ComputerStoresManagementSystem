6b7dba1f502b579d26419cccf2b6da035a9d0200
Krystyna Niemtsova <kniemtsova@gmail.com>
2023-05-31 18:44:51 +1200

    Added comments to the categoryFilter class.

--- src/categoryFilter.java
+++ src/categoryFilter.java
@@ -1,14 +1,23 @@
+/* This class enables the rows to be filtered by category */
+
 import javax.swing.*;
 
 public class categoryFilter extends RowFilter {
     private String category;
+
+    // Constructor to initialize the category
     public categoryFilter(String category) {
         setCategory(category);
     }
+
+    // Override the include() method to determine if a row should be included in the filter
         @Override
     public boolean include(Entry entry) {
+        // Compare the value of the first column (index 0) with the category
         return entry.getStringValue(0).equals(category);
     }
+
+    // Method to set the category
     public void setCategory(String category) {
         this.category = category;
     }
