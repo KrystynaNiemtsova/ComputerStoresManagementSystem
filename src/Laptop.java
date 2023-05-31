2aaa6685f6e367171a2b57675cc8fc025ee03672
Krystyna Niemtsova <kniemtsova@gmail.com>
2023-05-31 19:47:47 +1200

    Added comments to the Laptop class.

--- src/Laptop.java
+++ src/Laptop.java
@@ -1,10 +1,20 @@
+/*
+    This class is the Laptop class.
+    It is a child class of the DesktopPC parent class,
+    and holds information specific to laptops including
+    screen size.
+ */
 public class Laptop extends DesktopPC {
     private float screenSize;
+
+    // Constructor to initialize the Laptop object with the specific properties
     public Laptop(String category, String type, String ID, String brand, String CPU, int memorySize, int SSD,
-                  float ScreenSize, float price) {
+                  float screenSize, float price) {
         super(category, type, ID, brand, CPU, memorySize, SSD, price);
         setScreenSize(screenSize);
     }
+
+    // Getter and setter for the screenSize property
     public float getScreenSize() {
         return screenSize;
     }
