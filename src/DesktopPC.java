b9cf833bdf312bf6ec7f5e31ad2e6f8993f64dd2
Krystyna Niemtsova <kniemtsova@gmail.com>
2023-05-31 19:40:16 +1200

    Added comments to the DesktopPC class.

--- src/DesktopPC.java
+++ src/DesktopPC.java
@@ -1,17 +1,29 @@
+/*
+    This class is the DesktopPC class.
+    It is a child class of the device parent class,
+    and holds information specific to Desktop PC's including memory size and SSD.
+ */
+
 public class DesktopPC extends Device {
     private int memorySize;
     private int SSD;
+
+    // Constructor to initialize the DesktopPC object with the specific properties
     public DesktopPC(String category, String type, String ID, String brand, String CPU, int memorySize, int SSD, float price) {
         super(category, type, ID, brand, CPU, price);
         setMemorySize(memorySize);
         setSSD(SSD);
     }
+
+    // Getter and setter for the memory size property
     public int getMemorySize() {
         return memorySize;
     }
     public void setMemorySize(int memorySize) {
         this.memorySize = memorySize;
     }
+
+    // Getter and setter for the SSD property
     public int getSSD() {
         return SSD;
     }
