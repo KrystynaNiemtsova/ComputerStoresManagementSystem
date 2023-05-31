c91ee55e96276da99e774528099f4f7f19bc2aab
Krystyna Niemtsova <kniemtsova@gmail.com>
2023-05-31 17:35:22 +1200

    Changes logo and added comments to the main class.

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,21 @@
+/*This class runs the code */
+
 import java.io.File;
 public class Main {
     public static void main(String[] args) {
+        // Creating a new instance of DatabaseFileReader to read the file "computers.txt"
         new DatabaseFileReader(new File("computers.txt"));
+
+        // Setting user information for the LoginDialog
         setUserInformation();
+
+        // Creating a new instance of LoginScreen
         new LoginScreen();
     }
+
+    // Method to set user information in the LoginDialog
     private static void setUserInformation() {
+        // Setting users for the LoginDialog with their usernames, passwords, and roles
         LoginDialog.setUsers("p1", "p1", "Salesperson");
         LoginDialog.setUsers("p2", "p2", "Salesperson");
         LoginDialog.setUsers("p3", "p3", "Salesperson");
