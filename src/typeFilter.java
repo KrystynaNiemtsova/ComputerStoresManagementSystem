aa9da1d474a75606e92afec746edab820a5946d3
Krystyna Niemtsova <kniemtsova@gmail.com>
2023-05-31 20:00:32 +1200

    Added comments to the typeFilter class.

--- src/typeFilter.java
+++ src/typeFilter.java
@@ -1,12 +1,20 @@
+/*
+    This class enables the rows to be filtered by type.
+ */
+
 import javax.swing.*;
 
 public class typeFilter extends RowFilter {
     private String type;
     private String category;
+
+    // Constructor for the TypeFilter Class
     public typeFilter(String type, String category) {
         setType(type);
         setCategory(category);
     }
+
+    // Overriding the include() method of the RowFilter class
     @Override
     public boolean include (Entry entry) {
         if (entry.getStringValue(0).equals(category)) {
@@ -14,9 +22,13 @@ public class typeFilter extends RowFilter {
         }
         return false;
     }
+
+    // Setter for the type
     public void setType(String type) {
         this.type = type;
     }
+
+    // Setter for the category
     public void setCategory(String category) {
         this.category = category;
     }
