0580797c60253c447e00f0812654ddfb7e6cfd17
Krystyna Niemtsova <kniemtsova@gmail.com>
2023-05-31 19:58:25 +1200

    Added comments to the Tablet class.

--- src/Tablet.java
+++ src/Tablet.java
@@ -1,12 +1,23 @@
+/*
+    This class is the Tablet class.
+    It is a child class of the device parent class,
+    and holds information specific to tablets including screen size.
+ */
 public class Tablet  extends Device{
     private float screenSize;
+
+    // Constructor for the Tablet class
     public Tablet(String category, String type, String ID, String brand, String CPU, float screenSize, float price) {
         super(category, type, ID, brand, CPU, price);
         setScreenSize(screenSize);
     }
+
+    // Getter for the screen size
     public float getScreenSize() {
         return screenSize;
     }
+
+    // Setter for the screen size
     public void setScreenSize(float screenSize) {
         this.screenSize = screenSize;
     }
